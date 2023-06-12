16adc432d6dea4e294074c4c2c46d6de3353deed
Darlon Henrique <darlonhenrisouza@gmail.com>
2023-06-12 16:55:03 -0300

    feat: add suport to folder and also add formatation to json

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 const { promisify } = require('util');
 const { program } = require('commander');
+const path = require('path');
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
-async function Main(entry) {
+async function processFile(filePath) {
   try {
     // Read the JSON file
-    const data = await readFile(entry, 'utf8');
-    const fileName = entry.split('/').pop();
+    const data = await readFile(filePath, 'utf8');
+
+    if (!data || data.length === 0 || data === '' || data === '[]' || data === '{}' || data === 'null' || data === 'undefined') {
+      throw new Error(`File is empty: ${filePath}, skipping...`);
+    }
 
     // Parse the JSON data
     const jsonData = JSON.parse(data);
@@ -17,21 +21,52 @@ async function Main(entry) {
     // Get the first element of the array
     const firstElement = jsonData[0];
 
-    // create the folder output if it doesn't exist
+    // Create the output file path
+    const outputFileName = `output_${path.basename(filePath)}`;
+    const outputPath = path.join('output', outputFileName);
+
+    // Write the first element to the output file
+    await writeFile(outputPath, JSON.stringify(firstElement, null, 2));
+
+    console.log(`First element written to ${outputPath}`);
+  } catch (error) {
+    console.error(`error processing the file: ${filePath}:`, error);
+  }
+}
+
+async function Main(entry, options) {
+  try {
+    // Check if the entry is a file or a folder
+    const isFolder = options.folder;
+
+    // generate output folder if it doesn't exist
     if (!fs.existsSync('output')) {
       fs.mkdirSync('output');
     }
 
-    // Create the output file path
-    const outputPath = `output/${fileName}`;
+    // Handle single file path
+    if (!isFolder) {
+      await processFile(entry);
+      return;
+    }
 
-    // Write the first element to the output file
-    await writeFile(outputPath, JSON.stringify(firstElement));
+    // Handle folder path
+    const files = await fs.promises.readdir(entry);
 
-    console.log(`First element written to ${outputPath}`);
+    // Process each file individually
+    for (const file of files) {
+      if (path.extname(file) === '.json') {
+        const filePath = path.join(entry, file);
+        await processFile(filePath);
+      }
+    }
   } catch (error) {
     console.error('An error occurred:', error);
   }
 }
 
-program.arguments('<entry>').action(Main).parse(process.argv);
+program
+  .arguments('<entry>')
+  .option('-f, --folder', 'Specify folder instead of a single file')
+  .action(Main)
+  .parse(process.argv);
